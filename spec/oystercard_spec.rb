c310752055784ca6ba4fb86129ef2ce7d0a4c0a8
kotpsot <zygmunt.justyna@gmail.com>
2018-01-29 14:55:04 +0000

    ex. 4 - add initialize to Oystercard class and attrreader for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+
+subject(:oystercard) { described_class.new }
+
+  it 'has initial balance of zero' do
+    expect(oystercard.balance).to eq (0)
+  end
+
 end
